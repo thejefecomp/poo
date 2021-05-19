6625d5ad38ccf8b7b4ce2ff650ecdcd993861384
Jeferson Souza (thejefecomp) <jefecomp.official@gmail.com>
2021-05-19 17:24:46 +0000

    Hello World example (HelloWorld.java) added to the repository.

---
+++ examples/helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Esta classe especifica um exemplo de programa em Java que imprime a frase "Hello World" no console.
+*/
+
+public class HelloWorld{
+
+    public static void main(String ... args){
+
+        System.out.println("Hello World");
+
+    }
+
+}
