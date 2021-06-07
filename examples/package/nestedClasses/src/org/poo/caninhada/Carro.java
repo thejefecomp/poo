497f2ed077e13175f7cdd47c0ec106e3a86d559c
Jeferson Souza (thejefecomp) <jefecomp.official@gmail.com>
2021-06-07 17:47:22 +0000

    Work-In-Progress example of nested classes added to the repository.

---
+++ examples/package/nestedClasses/src/org/poo/caninhada/Carro.java
@@ -0,0 +1,110 @@
+package org.poo.caninhada;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*Esta classe representa a definição de um carro, a utilizar classes aninhadas. A classe roda é definida como classe membro da classe Carro.
+
+Nesta etapa, onde ainda não temos conhecimento dos conceitos de Generalização, Especialização (Realização), Agregação, e Composição, vamos trabalhar com uma implementação mais auto-contida. Voltaremos nesta implementação para separá-la em diferentes domínios quando os referidos conceitos forem abordados.
+
+Estado do exemplo: Work-in-progress.
+*/
+public class Carro{
+
+    public class Roda{
+
+        private String descricao;
+        
+        private Long numeroSerie;
+
+        private Integer tamanhoAro;
+
+        
+        public String getDescricao(){
+            return this.descricao;
+        }
+
+        public void setDescricao(String descricao){
+            this.descricao = descricao;
+        }
+
+        public Long getNumeroSerie(){
+            return this.numeroSerie;
+        }
+
+        public void setNumeroSerie(Long numeroSerie){
+            this.numeroSerie = numeroSerie;
+        }
+
+        public Integer getTamanhoAro(){
+            return this.tamanhoAro;
+        }
+
+        public void setTamanhoAro(Integer tamanhoAro){
+            this.tamanhoAro = tamanhoAro;
+        }
+
+    }
+
+    private String nome;
+
+    private List<Roda> listaRodas;
+
+    public boolean adicionaRoda(){
+        return this.adicionaRoda(null);
+    }
+
+    public boolean adicionaRoda(Roda roda){
+
+        if (this.listaRodas == null){
+            this.listaRodas = new ArrayList<>();
+        }
+        else if(roda == null){
+            roda = new Roda();
+
+            roda.setDescricao("Roda padrão POO aro 13");
+            roda.setTamanhoAro(13);
+            //Pensar em criar um número de série aleatório;
+            roda.setNumeroSerie(1234567890L);
+        }
+
+        return this.listaRodas.add(roda);
+    }
+
+    public boolean removeRoda(){
+        return this.removeRoda(null);
+    }
+
+    public boolean removeRoda(Long numeroSerie){
+
+        if(this.listaRodas == null || this.listaRodas.isEmpty())
+            return false;
+        else if (numeroSerie == null){
+            return this.listaRodas.remove(0) != null;
+        }
+
+        Roda rodaARemover = null;
+
+        for(var roda : this.listaRodas){
+            if (roda.getNumeroSerie() == numeroSerie){
+                rodaARemover = roda;
+                break;
+            }
+        }
+
+        return this.listaRodas.remove(rodaARemover);
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setListaRodas(List<Roda> listaRodas){
+        this.listaRodas = listaRodas;
+    }
+}
---
+++ examples/package/nestedClasses/src/org/poo/caninhada/main/Principal.java
@@ -0,0 +1,28 @@
+package org.poo.caninhada.main;
+
+
+import org.poo.caninhada.Carro;
+import org.poo.caninhada.Carro.Roda;
+
+/* Esta classe representa um exemplo de classe Principal, a declarar o método main,
+o qual é utilizado como base de execução de exemplos associados às classes aninhadas.
+
+Estado do exemplo: Work-in-progress.
+*/
+public class Principal{
+
+    public static void main(String ...args){
+
+        Carro carro = new Carro();
+        carro.setNome("Fusca");
+
+        Roda roda = carro.new Roda();
+
+        roda.setDescricao("Roda criada no main aro 13");
+        roda.setTamanhoAro(13);
+        roda.setNumeroSerie(1234568790L);
+
+        System.out.println(carro);
+        
+    }
+}
