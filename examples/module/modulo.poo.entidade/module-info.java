c1ad29b68cef104c76653300fd45cbb521391f60
Jeferson Souza (thejefecomp) <jefecomp.official@gmail.com>
2021-06-21 17:25:14 +0000

    Adding example of module declaration and usage to the repository.

---
+++ examples/module/modulo.poo.entidade/module-info.java
@@ -0,0 +1,9 @@
+/*
+Este arquivo declara o módulo chamado modulo.poo.entidade,
+a exportar o pacote org.poo.entidade para ser utilizado 
+pelo módulo chamado modulo.poo.sensacional.
+*/
+module modulo.poo.entidade{
+
+   exports org.poo.entidade;
+}
