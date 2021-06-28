e59c73d4bb11ba3a4d079f5ba6b0c110f5539b86
Jeferson Souza (thejefecomp) <jefecomp.official@gmail.com>
2021-06-28 17:26:32 +0000

    Adding an example of transitive depedency to the repository.

---
+++ examples/module/modulo.poo.interfaces/module-info.java
@@ -0,0 +1,10 @@
+/*
+Este arquivo declara o módulo chamado modulo.poo.interfaces,
+a exportar o pacote org.poo.interfaces.entidade para ser utilizado 
+pelo módulo chamado modulo.poo.entidade como dependência direta.
+*/
+module modulo.poo.interfaces{
+
+    exports org.poo.interfaces.entidade;
+
+}
--- examples/module/modulo.poo.sensacional/module-info.java
+++ examples/module/modulo.poo.sensacional/module-info.java
@@ -1,8 +1,8 @@
 /*
 Este arquivo declara o módulo chamado modulo.poo.sensacional,
-a ter como dependência o módulo chamado modulo.poo.entidade.
+a ter como dependência direta o módulo chamado modulo.poo.entidade,
+e como dependência indireta (transitiva) o módulo modulo.poo.interfaces.
 */
-
 module modulo.poo.sensacional{
 
     requires modulo.poo.entidade;
